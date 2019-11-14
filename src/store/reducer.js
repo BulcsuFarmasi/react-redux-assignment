ae31c3c39a9bdbbeb0a778f6d6c0dbdac4a3edd2
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-11-14 22:14:08 +0100

    add content to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,26 @@
-const initialState = {
-    persons: [];
-}
+import { ADD_PERSON, DELETE_PERSON } from "./actions";
 
-const reducer = (state  = initialState, action) {
-    return state;
-}
+const initialState = {
+  persons: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: "Max",
+        age: Math.floor(Math.random() * 40)
+      };
+      return {
+        persons: state.persons.concat(newPerson)
+      };
+    case DELETE_PERSON:
+      return {
+        persons: state.persons.filter(persons => person.id !== action.personId)
+      };
+  }
+  return state;
+};
 
 export default reducer;
